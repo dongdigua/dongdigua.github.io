7131eceaf6c2fdcd217ac1104455d0464e07f521
dongdigua <dongdigua@outlook.com>
2022-11-06 12:45:28 +0800

    posts.md index use list

--- js/random-quote.js
+++ js/random-quote.js
@@ -3,23 +3,28 @@ var data = [
     "解放思想, 实事求是, 与时俱进",
     "绿水青山就是金山银山",
     "为中华之崛起而读书!",
+    "减轻义务教育阶段学生作业负担、减轻校外培训负担",
 
     "<a id=quote href=https://www.bilibili.com/video/BV1vW411g7tN>谁甘人生黯淡无光 谁愿做叶不争艳芳</a>",
     "Fly high my friend, until we meet again",
+    "HsCnPtPYZnSr",
+    "There will be an answer: Let It Be",
 
     "The internet is FULL, go away!",
     "May the --force be with you",
-    "Software that sucks less",
-    "Software should be FREE!",
+    "<a id=quote href=https://suckless.org>Software that sucks less</a>",
     "whoami: No system is safe",
     "DROP it at <a id=quote href=https://lttstore.com>lttstore.com</a>",
-    "Can’t keep up! Is the server overloaded?\\n\nRunning 5000ms or 100 ticks behind",
+    "Can’t keep up! Is the server overloaded?\nRunning 5000ms or 100 ticks behind",
     "大炮做好了不放珍珠是什么意思?",
-    "write terrible nonsensical code to\\n\nensure the Copilot doesn't take over as captain",
+    "write terrible nonsensical code to\nensure the Copilot doesn't take over as captain",
     "Automagically",
     "<a id=quote href=https://www.curseforge.com/minecraft/mc-mods/carpet>NOT VANILLA!</a>",
     "Big Brother is Watching You",
-    "Deep Dark Fantacies",
+    "消除恐惧的最好办法就是面对恐惧,\n坚持, 才是胜利, 加油, 奥利给!",
+    "Presenting to the emergency room, unconcious",
+    "Never Gonna Give You Up",
+    "treat C compiler like your girlfriend",
 ];
 
 var index = Math.floor((Math.random() * data.length));
