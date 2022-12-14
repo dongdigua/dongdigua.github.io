a83120a41437855b2bae34f186902d88ef7dad3f
dongdigua <dongdigua@outlook.com>
2022-12-14 17:43:57 +0800

    more collections, CSS: title font
    - [RM] Herman
    - [RM] tildeverse
    - [RM] RMS C intro mail
    - isopenbsdsecu.re
    - LITCAVE
    - Security & Privacy Evaluations
    - Global interactive Emacs functions
    - Computerphile: Quantum & Brain-Like Computing

--- js/random-quote.js
+++ js/random-quote.js
@@ -11,7 +11,6 @@ var data = [
     "HsCnPtPSrYZn",
     "李无用地瓜碱水, 硒钫铕𬭶鿔合金",
     "There will be an answer: Let It Be",
-    "<a id=quote href=https://calm.com>Take a deep breath</a>",
     "我这人有几个缺点: 第一, 不会数数; 第二, 记性不好; 第四, 不会数数",
     "圈纸拿回家研磨去了",
     "我们从这里启航♫",
@@ -36,6 +35,7 @@ var data = [
     "<a id=quote href=damn_tencent_meeting>有什么业务是腾讯不做的吗?!</a>",
     "Rewrite in Rust!",
     "Your text editor runs in a web browser,\nMy web browser runs inside a text editor,\nWe are not the same",
+    "<a id=quote href=https://www.gnunet.org>It’s the metadata, stupid</a>",
 ];
 
 var index = Math.floor((Math.random() * data.length));
