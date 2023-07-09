069ff4817c00c3e4da4c9a1e1a3fb43eb7fcd4e3
dongdigua <dongdigua@outlook.com>
2023-07-09 18:59:02 +0800

    docker: apk --no-cache, git checkout

--- gmi/docker/update-git.sh
+++ gmi/docker/update-git.sh
@@ -4,4 +4,6 @@ cd /dongdigua.github.io
 git pull --rebase >> /var/log/git.log 2>&1
 python misc/mdlist2gmi.py > posts.gmi
 cp -r gmi/docker/cgi .
+rm index.gmi
+git checkout -- index.gmi
 pkill -HUP gmid
