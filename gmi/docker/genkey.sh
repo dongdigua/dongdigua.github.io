197a1056ab856b8f1a9d8af8f3bf1de3a86c7942
dongdigua <dongdigua@outlook.com>
2023-07-12 16:22:35 +0800

    docker: change ssl volume path
    otherwise ca-certificate will be missing
    and it can't install package through https

--- gmi/docker/genkey.sh
+++ gmi/docker/genkey.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-if test -f /etc/ssl/gmid.pem; then
+if test -f /ssl/gmid.pem; then
     echo "OK certificate exists"
 else
     echo "generating certificate..."
-    apk add libressl
+    mkdir /ssl
+    apk add --no-cache libressl
     libressl req -x509 -newkey rsa:4096 -nodes \
-        -keyout /etc/ssl/private/gmid.key \
-        -out /etc/ssl/gmid.pem \
+        -keyout /ssl/private/gmid.key \
+        -out /ssl/gmid.pem \
         -days 365 -subj "/CN=$GEMINI_HOST"
 fi
